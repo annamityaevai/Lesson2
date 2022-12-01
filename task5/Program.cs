8408a92d0f3d373f20f0119e134fc758a6cc7427
Anna Mityaeva <aiisa@inbox.ru>
2022-12-01 08:02:04 +0300

    task 5

---
+++ task5/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Insert number a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert number b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine("yes");
+}
+else if (b == a * a)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
