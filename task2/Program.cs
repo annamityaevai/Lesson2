b5662046b2d47cd730e5f5f48c37286e33e7a5d6
Anna Mityaeva <aiisa@inbox.ru>
2022-12-01 07:32:19 +0300

    task 2

---
+++ task2/Program.cs
@@ -0,0 +1,7 @@
+﻿Random random = new Random();
+int number = random.Next(99, 1000);
+Console.WriteLine("Random number = {0}", number);
+int a = number / 100;
+int c = number % 10;
+Console.Write($"{a}");
+Console.Write($"{c}");
