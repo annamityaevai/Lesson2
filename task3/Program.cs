678b5c1359baa78e0be87e6af79a7672393280fe
Anna Mityaeva <aiisa@inbox.ru>
2022-12-01 07:45:09 +0300

    task 3

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Insert number a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert number b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a % b == 0)
+{
+    Console.WriteLine("a is mulriple of b");
+}
+else
+{
+    int c = a % b;
+    Console.WriteLine("a is not multiple of b, remainder = " + c);
+}
